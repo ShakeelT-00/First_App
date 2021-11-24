26a4636bdb9aa510b311f5ac92cb37641b6a3c7e
shakeelt <shakeelt@mcmaster.ca>
2021-11-24 15:00:32 -0500

    Initial commit

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onBtnClick(View view) {
+        TextView txtFirstName = findViewById(R.id.txtFirstName);
+        TextView txtLastName = findViewById(R.id.txtLastName);
+        TextView txtEmail = findViewById(R.id.txtEmail);
+
+        EditText edtTxtName = findViewById(R.id.edtTxtName);
+        EditText edtTxtLastName = findViewById(R.id.edtTxtLastname);
+        EditText edtTxtEmail = findViewById(R.id.edtTxtEmail);
+
+        txtFirstName.setText("First Name: " + edtTxtName.getText().toString());
+        txtLastName.setText("Last Name: " + edtTxtLastName.getText().toString());
+        txtEmail.setText("Email: " + edtTxtEmail.getText().toString());
+    }
+}
